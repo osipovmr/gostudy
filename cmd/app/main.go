e810b1ce5b242f71813823fe074b4fb8e332eadb
Осипов Илья Александрович <osipovmr@yandex.ru>
2026-04-26 16:37:19 +0300

    создание топиков

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"log/slog"
+	"time"
 
 	"os/signal"
 	"syscall"
@@ -11,6 +12,8 @@ import (
 	"gostudy/internal/config"
 
 	"gostudy/internal/logger"
+
+	"github.com/segmentio/kafka-go"
 )
 
 func main() {
@@ -26,6 +29,36 @@ func main() {
 		slog.Error("failed to init app", "error", err)
 		return
 	}
+
+	dialer := &kafka.Dialer{Timeout: 10 * time.Second}
+	conn, err := dialer.DialContext(ctx, "tcp", cfg.KAFKAAddr)
+	if err != nil {
+		slog.Error(err.Error())
+	}
+	defer conn.Close()
+
+	err = conn.CreateTopics(
+		kafka.TopicConfig{
+			Topic:             "__consumer_offsets",
+			NumPartitions:     1,
+			ReplicationFactor: 1,
+		},
+	)
+	if err != nil {
+		slog.Error(err.Error())
+	}
+	err = conn.CreateTopics(
+		kafka.TopicConfig{
+			Topic:             "registration_topic",
+			NumPartitions:     1,
+			ReplicationFactor: 1,
+		},
+	)
+	if err != nil {
+		slog.Error(err.Error())
+	}
+
+	slog.Info("topic created")
 
 	if err := application.Run(ctx); err != nil {
 		slog.Error("app stopped with error", "error", err)
